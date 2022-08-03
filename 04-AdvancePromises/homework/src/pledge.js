eff8cd32246e2dc002de5af4ac7f59ed7d560bdd
emuller1996 <stefano.muller@hotmail.com>
2022-08-03 10:30:46 -0500

    finish hw Promises

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -1,56 +1,127 @@
-'use strict';
+"use strict";
 /*----------------------------------------------------------------
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
 
-function $Promise (executor){
- if(typeof executor !== 'function') throw TypeError('executor not is a function');
- this._state = 'pending';
- this._value = null;
+function $Promise(executor) {
+  if (typeof executor !== "function")
+    throw TypeError("executor not is a function");
+  this._state = "pending";
+  this._value = undefined;
+  this._handlerGroups = [];
 
- this._handlerGroups = [];
-
-
- this._callHandlers = ()=> {
-    while(this._handlerGroups.length){
-        var currentHandler = this._handlerGroups.shift();
-        if(this._state === 'fulfilled'){
-            currentHandler.successCb(this._value);
-        }else{
-            currentHandler.errorCb(this._value);
-        }
-    }
- };
-
- this._internalResolve = (data)=>{
-    if(this._state === 'pending')  this._value =data;
-    if(this._state != 'rejected') this._state='fulfilled'
-    this._callHandlers()
- };
-
- this._internalReject = (data)=>{
-    if(this._state === 'pending')  this._value =data;
-    if(this._state != 'fulfilled')  this._state='rejected';
-    this._callHandlers()
- };
-
- this.then = (successCb,errorCb)=>{
-    if(typeof successCb ==='function' && typeof errorCb ==='function'){
-        this._handlerGroups.push({
-            successCb,
-            errorCb
-        })
-    }else{
-        this._handlerGroups.push({
-            successCb:false,
-            errorCb:false
-        })
-    }
-    if (this._state !== 'pending')  this._callHandlers();
- } 
+  executor(this._internalResolve.bind(this), this._internalReject.bind(this));
 }
 
+$Promise.prototype._internalResolve = function (data) {
+  if (this._state === "pending") {
+    this._value = data;
+    this._state = "fulfilled";
+    this._callHandlers();
+  }
+};
+
+$Promise.prototype._internalReject = function (data) {
+  if (this._state === "pending") {
+    this._value = data;
+    this._state = "rejected";
+    this._callHandlers();
+  }
+};
+
+$Promise.prototype.then = function (successCb, errorCb) {
+  if (typeof successCb !== "function") successCb = false;
+  if (typeof errorCb !== "function") errorCb = false;
+  const downstreamPromise = new $Promise(function() {})
+
+
+  this._handlerGroups.push({
+    successCb,
+    errorCb,
+    downstreamPromise
+  });
+
+  if (this._state !== "pending") this._callHandlers();
+  return downstreamPromise; // promesa B 
+};
+
+
+
+$Promise.prototype._callHandlers = function() {
+    while(this._handlerGroups.length) {
+        let currentHandler = this._handlerGroups.shift(); //{successCB, errrCB}
+
+        //Promesa B
+        const downstreamPromise = currentHandler.downstreamPromise;
+
+        if(this._state === 'fulfilled') {
+            if(!currentHandler.successCb) {
+                //No tenemos successHanlder
+                //Resuelvo a B al valor de A
+                downstreamPromise._internalResolve(this._value) //promesaA.value
+            }
+            else {
+                //Hay succes Handler
+                try {
+                    const result = currentHandler.successCb(this._value) // valor / promesa / err
+
+                    //1) SuccessCb retorna una promesa (Z)
+                    if(result instanceof $Promise ) {
+                        result.then(function(value) {
+                            downstreamPromise._internalResolve(value)
+                        }, function(err) {
+                            downstreamPromise._internalReject(err)
+                        })
+                    }
+                    else {
+                        //2) SuccessCb retorna un valor
+                        downstreamPromise._internalResolve(result) // un valor
+                    }
+                } catch (error) {
+                    //3) El handler successCb lanzó un error
+                    downstreamPromise._internalReject(error)
+                }
+            }
+        }          
+
+        else {
+            //Cuando la promesa A se rechace
+            if(!currentHandler.errorCb) {
+                //Y no tenemos hanlder
+                downstreamPromise._internalReject(this._value) //valor de promesa A
+            }
+            //Tenemos handler
+            else {
+                try {
+                    const result = currentHandler.errorCb(this._value) //promesa | err | valor(err)
+
+                    //1) ErrorCb retorna una promesa(Z)
+                    if(result instanceof $Promise ) {
+                        result.then(function(value){
+                            //Promesa B la resuelvo al valor de Z
+                            downstreamPromise._internalResolve(value)
+                        }, function(err) {
+                            downstreamPromise._internalReject(err)
+                        })
+                    }
+                    else {
+                        //2) ErrorCb no devolvia una promesa. Retorna un valor
+                        //Promesa B se resuelve al motivo de A
+                        downstreamPromise._internalResolve(result)
+                    }
+                } catch (error) {
+                    //3) El handler lanzó un error
+                    downstreamPromise._internalReject(error)
+                }
+            }
+        }
+    }
+}
+
+$Promise.prototype.catch = function(errorCb){
+    return this.then(null,errorCb);
+  }
 
 module.exports = $Promise;
 /*-------------------------------------------------------
